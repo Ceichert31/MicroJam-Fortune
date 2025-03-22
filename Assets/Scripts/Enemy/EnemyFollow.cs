94386725aa14912a9394ac74f9841183a60641b4
Brody Silva <brody.silva@mymail.champlain.edu>
2025-03-22 00:00:50 -0400

    feat: added bat animations

--- Assets/Scripts/Enemy/EnemyFollow.cs
+++ Assets/Scripts/Enemy/EnemyFollow.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class EnemyFollow : MonoBehaviour, IDamageable
 {
     [SerializeField] private float chaseRadius = 5f;
     [SerializeField] private float speed = 2f;
     [SerializeField] private int enemyHealth = 3;
+    private Animator animator;
+    private SpriteRenderer enemyRenderer;
 
     private Transform playerTransform => GameManager.Instance.PlayerTransform;
 
     private void Awake()
     {
-        // empty
+        animator = GetComponent<Animator>();
+        enemyRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
 
+    private Vector3 enemyToPlayer;
     private void Update()
     {
+        enemyToPlayer = transform.position - playerTransform.position;
         if (playerTransform != null && Vector3.Distance(transform.position, playerTransform.position) <= chaseRadius)
         {
             transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
