1d422d3df802672277602f9614eac457b5260cbe
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-03-22 19:06:33 -0400

    feat: Player flashing

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -1,27 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour, IDamageable
 {
     [SerializeField] private int currentHealth;
-    [SerializeField] private float iFrameDuration = 0.8f;
+    [SerializeField] private int iFrameDuration;
+    [SerializeField] private float spriteFlashInterval = 0.8f;
     [SerializeField] private FloatEventChannel removeHealthUI_Event;
     [SerializeField] private FloatEventChannel addHealthUI_Event;
     private int maxHealth => GameManager.Instance.MaxHealth;
 
-    private bool hasIFrames;
+    private SpriteRenderer playerRenderer;
+
+    private bool isDamageable = true;
 
     private void Start()
     {
         currentHealth = maxHealth;
+
+        playerRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
 
     public void DealDamage(int damage)
     {
         //Prevent damage
-        if (hasIFrames) return;
-
-        hasIFrames = true;
-        Invoke(nameof(ResetIFrames), iFrameDuration);
+        if (!isDamageable) return;
 
         //Deal damage
         currentHealth -= damage;
@@ -30,15 +34,32 @@ public class Health : MonoBehaviour, IDamageable
         //Send current health
         removeHealthUI_Event.CallEvent(new FloatEvent(damage));
 
+        //death
         if (currentHealth <= 0) 
         {
             PlayerDeath();
+            Time.timeScale = 0;
         }
-    }
 
-    private void ResetIFrames()
+        StartCoroutine(StartIFrames());
+    }
+    IEnumerator StartIFrames()
     {
-        hasIFrames = false;
+        //disable player damage here
+        isDamageable = false;
+
+        float flashNumber = 0;
+        while (flashNumber < iFrameDuration)
+        {
+            playerRenderer.enabled = false;
+            yield return new WaitForSeconds(spriteFlashInterval);
+            playerRenderer.enabled = true;
+            flashNumber++;
+            yield return new WaitForSeconds(spriteFlashInterval);
+        }
+
+        //Reenable player damage here
+        isDamageable = true;
     }
 
     private void PlayerDeath()
