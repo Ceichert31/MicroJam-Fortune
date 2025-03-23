617caf31ec3ef54a497f02c785c719fed1364ead
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-03-23 17:01:45 -0400

    feat: Saving/Loading progress

--- Assets/Scripts/Utility/SceneCheck.cs
+++ Assets/Scripts/Utility/SceneCheck.cs
@@ -6,19 +6,7 @@ public class SceneCheck : MonoBehaviour
     [SerializeField] private PlayerBaseStats baseStats;
     private void Awake()
     {
-        switch (SceneManager.GetActiveScene().buildIndex)
-        {
-            //Stats
-            case 1:
-                GameManager.Instance.SetStats(new (baseStats));
-                PlayerDataManager.Instance.Save();
-                break;
-
-                //Game
-            case 2:
-                //Load data on main game scene start
-                PlayerDataManager.Instance.Load();
-                break;
-        }
+        GameManager.Instance.SetStats(new(baseStats));
+        PlayerDataManager.Instance.Save();
     }
 }
