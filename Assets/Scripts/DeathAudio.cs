9c30b2cc6f3568741aeb88c1ab598969e5a23a4d
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-03-23 13:40:35 -0400

    feat: Bag SFX

--- Assets/Scripts/DeathAudio.cs
+++ Assets/Scripts/DeathAudio.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class DeathAudio : MonoBehaviour
 {
     [SerializeField] private AudioPitcherSO deathAudio;
-    [SerializeField] private AudioSource source;
+    private AudioSource source;
     [SerializeField] private float lifetime = 6f;    
     private void Start()
     {
+        source = GetComponent<AudioSource>();
         deathAudio.Play(source);
         Invoke(nameof(DestroyObject), lifetime);
     }
