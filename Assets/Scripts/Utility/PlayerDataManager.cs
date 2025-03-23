b4a9b60b7ae046bbfd23270175dec1415aacf8c0
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-03-23 19:20:45 -0400

    feat: Switched to player prefs

--- Assets/Scripts/Utility/PlayerDataManager.cs
+++ Assets/Scripts/Utility/PlayerDataManager.cs
@@ -19,26 +19,53 @@ public class PlayerDataManager : MonoBehaviour
     /// </summary>
     public void Save()
     {
+        //playerStats = GameManager.Instance.PlayerStats;
+        ////Paths
+        //string json = JsonUtility.ToJson(playerStats);
+        //string path = Application.persistentDataPath + "/playerData.json";
+
+        ////write
+        //System.IO.File.WriteAllText(path, json);
+
         playerStats = GameManager.Instance.PlayerStats;
-        //Paths
-        string json = JsonUtility.ToJson(playerStats);
-        string path = Application.persistentDataPath + "/playerData.json";
-        
-        //write
-        System.IO.File.WriteAllText(path, json);
+        PlayerPrefs.SetInt("Health", playerStats.maxHealth);
+        PlayerPrefs.SetInt("Speed", playerStats.movementSpeed);
+        PlayerPrefs.SetInt("Damage", playerStats.attackDamage);
+        PlayerPrefs.SetInt("Capacity", playerStats.carryingCapacity);
+
+        PlayerPrefs.SetInt("Vision", playerStats.vision);
+        PlayerPrefs.SetInt("Confidence", playerStats.confidence);
+        PlayerPrefs.SetInt("Luck", playerStats.luck);
+        PlayerPrefs.SetInt("Swag", playerStats.swag);
+        PlayerPrefs.SetInt("Agility", playerStats.agility);
     }
 
     public void Load()
     {
-        string path = Application.persistentDataPath + "/playerData.json";
+        //string path = Application.persistentDataPath + "/playerData.json";
 
-        if (File.Exists(path))
-        {
-            string json = System.IO.File.ReadAllText(path);
-            PlayerStats loadedStats = JsonUtility.FromJson<PlayerStats>(json);
+        //if (File.Exists(path))
+        //{
+        //    string json = System.IO.File.ReadAllText(path);
+        //    PlayerStats loadedStats = JsonUtility.FromJson<PlayerStats>(json);
 
-            //Set stats
-            GameManager.Instance.SetStats(loadedStats);
-        }
+        //    //Set stats
+        //    GameManager.Instance.SetStats(loadedStats);
+        //}
+
+        PlayerStats stats;
+
+        stats.maxHealth = PlayerPrefs.GetInt("Health");
+        stats.movementSpeed = PlayerPrefs.GetInt("Speed");
+        stats.attackDamage = PlayerPrefs.GetInt("Damage");
+        stats.carryingCapacity = PlayerPrefs.GetInt("Capacity");
+
+        stats.vision = PlayerPrefs.GetInt("Vision");
+        stats.confidence = PlayerPrefs.GetInt("Confidence");
+        stats.luck = PlayerPrefs.GetInt("Luck");
+        stats.swag = PlayerPrefs.GetInt("Swag");
+        stats.agility = PlayerPrefs.GetInt("Agility");
+
+        GameManager.Instance.SetStats(stats);
     }
 }
