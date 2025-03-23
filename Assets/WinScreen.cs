7867db9ff31749dee620fa4ebc54368bc5eb29c0
Matthew <matthew.oneill@mymail.champlain.edu>
2025-03-23 17:52:30 -0400

    feat: set tutorial to default unopen

--- Assets/WinScreen.cs
+++ Assets/WinScreen.cs
@@ -5,7 +5,7 @@ public class WinScreen : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PlayerPrefs.SetInt("hardModeUnlocked", 1);
     }
 
     // Update is called once per frame
