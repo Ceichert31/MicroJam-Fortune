c0ead50c57d2bf50fb3e740f15aa01f24beb90ea
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-03-23 21:39:37 -0400

    Revert "fix: Save fix"
    This reverts commit 97bff95a0fe8af4dc6f281939c7ef79a7b0ce578.

--- Assets/Scripts/Utility/PlayerDataManager.cs
+++ Assets/Scripts/Utility/PlayerDataManager.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerDataManager : MonoBehaviour
@@ -13,8 +12,6 @@ public class PlayerDataManager : MonoBehaviour
             Destroy(this);
         else
             Instance = this;
-
-        DontDestroyOnLoad(gameObject);
     }
 
     /// <summary>
@@ -31,18 +28,16 @@ public class PlayerDataManager : MonoBehaviour
         //System.IO.File.WriteAllText(path, json);
 
         playerStats = GameManager.Instance.PlayerStats;
-        //PlayerPrefs.SetInt("Health", playerStats.maxHealth);
-        //PlayerPrefs.SetInt("Speed", playerStats.movementSpeed);
-        //PlayerPrefs.SetInt("Damage", playerStats.attackDamage);
-        //PlayerPrefs.SetInt("Capacity", playerStats.carryingCapacity);
+        PlayerPrefs.SetInt("Health", playerStats.maxHealth);
+        PlayerPrefs.SetInt("Speed", playerStats.movementSpeed);
+        PlayerPrefs.SetInt("Damage", playerStats.attackDamage);
+        PlayerPrefs.SetInt("Capacity", playerStats.carryingCapacity);
 
-        //PlayerPrefs.SetInt("Vision", playerStats.vision);
-        //PlayerPrefs.SetInt("Confidence", playerStats.confidence);
-        //PlayerPrefs.SetInt("Luck", playerStats.luck);
-        //PlayerPrefs.SetInt("Swag", playerStats.swag);
-        //PlayerPrefs.SetInt("Agility", playerStats.agility);
-
-        //PlayerPrefs.Save();
+        PlayerPrefs.SetInt("Vision", playerStats.vision);
+        PlayerPrefs.SetInt("Confidence", playerStats.confidence);
+        PlayerPrefs.SetInt("Luck", playerStats.luck);
+        PlayerPrefs.SetInt("Swag", playerStats.swag);
+        PlayerPrefs.SetInt("Agility", playerStats.agility);
     }
 
     public void Load()
@@ -58,19 +53,19 @@ public class PlayerDataManager : MonoBehaviour
         //    GameManager.Instance.SetStats(loadedStats);
         //}
 
-        //PlayerStats stats;
+        PlayerStats stats;
 
-        //stats.maxHealth = PlayerPrefs.GetInt("Health");
-        //stats.movementSpeed = PlayerPrefs.GetInt("Speed");
-        //stats.attackDamage = PlayerPrefs.GetInt("Damage");
-        //stats.carryingCapacity = PlayerPrefs.GetInt("Capacity");
+        stats.maxHealth = PlayerPrefs.GetInt("Health");
+        stats.movementSpeed = PlayerPrefs.GetInt("Speed");
+        stats.attackDamage = PlayerPrefs.GetInt("Damage");
+        stats.carryingCapacity = PlayerPrefs.GetInt("Capacity");
 
-        //stats.vision = PlayerPrefs.GetInt("Vision");
-        //stats.confidence = PlayerPrefs.GetInt("Confidence");
-        //stats.luck = PlayerPrefs.GetInt("Luck");
-        //stats.swag = PlayerPrefs.GetInt("Swag");
-        //stats.agility = PlayerPrefs.GetInt("Agility");
+        stats.vision = PlayerPrefs.GetInt("Vision");
+        stats.confidence = PlayerPrefs.GetInt("Confidence");
+        stats.luck = PlayerPrefs.GetInt("Luck");
+        stats.swag = PlayerPrefs.GetInt("Swag");
+        stats.agility = PlayerPrefs.GetInt("Agility");
 
-        GameManager.Instance.SetStats(playerStats);
+        GameManager.Instance.SetStats(stats);
     }
 }
